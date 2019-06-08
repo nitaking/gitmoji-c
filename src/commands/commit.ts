6213e5bbf28dc2d365794fa4cf3910a6cc856e0a
nitaking <10850034+nitaking@users.noreply.github.com>
2019-06-08 22:23:47 +0900

    :sparkles: Add: commit
    - install: enquirer, execa, listr

---
+++ src/commands/commit.ts
@@ -0,0 +1,60 @@
+import {Command, flags} from '@oclif/command'
+import {prompt} from 'enquirer'
+import * as execa from 'execa'
+
+interface IAnswer {
+  gitmoji: string;
+  title: string;
+  message: string;
+}
+
+export default class Hello extends Command {
+  static description = 'Interactively commit using the prompts'
+
+  async run() {
+    // const {args, flags} = this.parse(Hello)
+
+    const answer: IAnswer = await prompt([
+      {
+        type: 'input',
+        name: 'gitmoji',
+        message: 'Choose a gitmoji:', // todo: sourceにして、gitmojiAPIの結果を載せる
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the commit title',
+      },
+      {
+        type: 'input',
+        name: 'message',
+        message: 'Enter the commit message:',
+      },
+    ])
+
+    const title = `${answer.gitmoji} ${answer.title}`
+    const body = `${answer.message}`
+
+    try {
+      const result = await this.commit(title, body)
+      this.log(result)
+    } catch (e) {
+      this.errorMessage(e)
+    }
+  }
+
+  private async commit(title: string, message: string) {
+    const {stdout} = await execa('git', [
+      'commit',
+      '-m',
+      title,
+      '-m',
+      message,
+    ])
+    return stdout
+  }
+
+  private errorMessage(message: string) {
+    this.error(message, {exit: 2})
+  }
+}
